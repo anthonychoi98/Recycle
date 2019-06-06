2681f1cd46b9bea0e1f55d7079d3fc1d35939187
Anthony Choi <froshiga@gmail.com>
2019-06-06 12:40:50 -0700

    basic login/logout etc working.. just need to make it loook niice and purrty

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.button import Button, ButtonBehavior
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle
 from kivy.uix.boxlayout import BoxLayout
@@ -11,8 +11,12 @@ from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from myfirebase import MyFirebase
+from kivy.uix.image import Image
+from kivy.uix.anchorlayout import AnchorLayout
+import json
+import requests
 
-class MainAppWindow(Screen):
+class HomeWindow(Screen):
     pass
 
 class LoginWindow(Screen):
@@ -21,7 +25,13 @@ class LoginWindow(Screen):
 class SignUpWindow(Screen):
     pass
 
+class NameWindow(Screen):
+    pass
+
 class WindowManager(ScreenManager):
+    pass
+
+class ImageButton(ButtonBehavior, Image):
     pass
 
 sm = Builder.load_file("my.kv")
@@ -29,10 +39,79 @@ sm = Builder.load_file("my.kv")
 LabelBase.register(name="oj", fn_regular="orange juice 2.0.ttf")
 
 class MyApp(App):
+    refresh_token_file = "refresh_token.txt"
+    wapik = "AIzaSyBvGJKPt0u1NGMku6SqBQwKCcD87QSG23w"
 
     def build(self):
-        self.my_firebase = MyFirebase
+        self.my_firebase = MyFirebase()
         return sm
+
+    def on_start(self):
+        try:
+            with open(self.refresh_token_file, 'r') as f:
+                refresh_Token = f.read()
+            id_Token, local_Id = self.my_firebase.exchange_refresh_token(refresh_Token)
+            self.local_id = local_Id
+            self.id_token = id_Token
+
+            result = requests.get('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token)
+            data = json.loads(result.content.decode())
+
+            cashMoney = data['recycled']
+            self.cashMoney = cashMoney
+            status = data['status']
+            self.status = status
+            self.root.ids["home_screen"].ids["recycle_label"].text = str(cashMoney)
+            self.root.ids['home_screen'].ids['status'].text = status
+
+            self.root.current="home_screen"
+        except:
+            pass
+
+    def init_status(self, stats):
+            self.root.ids["home_screen"].ids['status'].text = stats
+
+    def tinCanCount(self):
+        #increment how much tin_cash money they got
+
+        #print(self.local_Id)
+        result = requests.get('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token)
+        data = json.loads(result.content.decode())
+        cash_M = data["recycled"]
+        cM = int(cash_M) + 1
+        cashMoneyPatch = '{"recycled": %s}'% str(cM)
+        requests.patch('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token, data=cashMoneyPatch)
+        self.root.ids["home_screen"].ids["recycle_label"].text = str(cM)
+
+    def glassBottleCount(self):
+        print(self.local_id)
+        result = requests.get('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token)
+        data = json.loads(result.content.decode())
+        cash_M = data["recycled"]
+        cM = int(cash_M) + 2
+        cashMoneyPatch = '{"recycled": %s}'% str(cM)
+        requests.patch('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token, data=cashMoneyPatch)
+        self.root.ids["home_screen"].ids["recycle_label"].text = str(cM)
+
+    def setNickname(self, u_name):
+        they_name = str(u_name)
+        print(they_name)
+        result = requests.get('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token)
+        data = json.loads(result.content.decode())
+        namePatch = '{"nickname": %s}'% they_name
+        requests.patch('https://recyclingapp-44e68.firebaseio.com/' + self.local_id + '.json?auth=' + self.id_token, data=namePatch)
+        self.root.ids["home_screen"].ids["nickname"].text = they_name
+
+    def sign_out(self):
+        #app = App.get_running_app()
+
+        with open(self.refresh_token_file, 'w') as f:
+            f.write("")
+
+        self.root.current="login_screen"
+
+        self.root.ids.login_screen.ids.username.text = ""
+        self.root.ids.login_screen.ids.pwd.text = ""
 
 
 recycle = MyApp()
--- myfirebase.py
+++ myfirebase.py
@@ -5,6 +5,7 @@ from kivy.app import App
 class MyFirebase():
 
     wapik = "AIzaSyBvGJKPt0u1NGMku6SqBQwKCcD87QSG23w" #web API key
+    refresh_token_file = "refresh_token.txt"
 
     #def patch(self, JSON):
         #to_database = json.loads(JSON)
@@ -12,28 +13,102 @@ class MyFirebase():
 
     def sign_up(self, email, password):
         #firebase returns localID,authID,and a refreshToken
+        app = App.get_running_app()
         signup_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBvGJKPt0u1NGMku6SqBQwKCcD87QSG23w"
 
-        signup_data = {"email": email,
+        signup_payload = {"email": email,
                         "password":password,
                         "returnsecureToken": True,
                         }
 
-        signup_request = requests.post(url=signup_url, data=signup_data)
+        signup_request = requests.post(url=signup_url, data=signup_payload)
         print(signup_request.ok)
         print(signup_request.content.decode())
+        sign_up_data = json.loads(signup_request.content.decode())
 
         if signup_request.ok == True:
+            error_message = "Success!"
+            App.get_running_app().root.ids['signup'].ids['signin_message'].text = error_message
+            refresh_token = sign_up_data['refreshToken']
+            localId = sign_up_data['localId']
+            idToken = sign_up_data['idToken']
             print("created account!")
+
+            with open("refresh_token.txt", "w") as f:
+                f.write(refresh_token)
+
+            app.local_id = localId
+            app.id_token = idToken
+
+            my_payload = '{"nickname": "", "avatar": "image0.jpg", "status": "noob", "recycled": "0"}'
+            post_request = requests.patch("https://recyclingapp-44e68.firebaseio.com/" + localId + ".json?auth=" + idToken,
+            data=my_payload)
+            print(post_request.ok)
+            print(json.loads(post_request.content.decode()))
+
+            app.root.current = "name_screen"
+
+            #app.root.current = "home_screen"
+
 
         if signup_request.ok == False:
             error_data = json.loads(signup_request.content.decode())
             error_message = error_data["error"]["message"]
             print(error_message)
-            App.get_running_app().root.ids['signup'].ids['signin_message'].text = error_message
-
+            app.root.ids['signup'].ids['signin_message'].text = error_message
         pass
 
+    def exchange_refresh_token(self, refresh_Token):
+        print("hello/")
+        refresh_url = "https://securetoken.googleapis.com/v1/token?key=AIzaSyBvGJKPt0u1NGMku6SqBQwKCcD87QSG23w"
+        refresh_payload = '{"grant_type": "refresh_token", "refresh_token": "%s"}'% refresh_Token
+        refresh_req = requests.post(refresh_url, data=refresh_payload)
 
-    def sign_in(self):
-        pass
+        id_token = refresh_req.json()['id_token']
+        local_id = refresh_req.json()['user_id']
+
+        return id_token, local_id
+
+
+    def sign_in(self, email, password):
+        signin_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + self.wapik
+        signin_payload = {"email": email, "password": password, "returnSecureToken": True}
+        signin_request = requests.post(signin_url, data=signin_payload)
+        sign_up_data = json.loads(signin_request.content.decode())
+        app = App.get_running_app()
+
+        if signin_request.ok == False:
+            error_message = sign_up_data["error"]["message"]
+            print(error_message)
+            app.root.ids['login_screen'].ids['login_message'].text = error_message
+
+        if signin_request.ok == True:
+            print("alright alright alright alright alright ok")
+            refresh_token = sign_up_data['refreshToken']
+            localId = sign_up_data['localId']
+            idToken = sign_up_data['idToken']
+            # Save refreshToken to a file
+            with open(app.refresh_token_file, "w") as f:
+                f.write(refresh_token)
+
+            # Save localId to a variable in main app class
+            # Save idToken to a variable in main app class
+            app.local_id = localId
+            app.id_token = idToken
+
+            result = requests.get('https://recyclingapp-44e68.firebaseio.com/' + app.local_id + '.json?auth=' + app.id_token)
+            data = json.loads(result.content.decode())
+            cM = data["recycled"]
+            nname = data["nickname"]
+            stat = data["status"]
+
+            app.root.ids["home_screen"].ids["recycle_label"].text = str(cM)
+            app.root.ids["home_screen"].ids["status"].text = stat
+            app.root.ids["home_screen"].ids["nickname"].text = nname
+
+            app.on_start()
+
+
+
+
+        #ok
